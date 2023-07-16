5eea53b9cbe8ecc4470c1534cd0d31406d1ed82a
Simin Cao <simin.cao@outlook.com>
2023-07-16 15:09:32 +0200

    fix errors

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with TelegramClient(config['user']['session'], config['user']['api_id'], config[
 
    @client.on(events.NewMessage(pattern='(?i).*(Hello|自动回复)'))
    async def handler(event):
-      await event.reply('Hey! You have reached out to the automated bot answer, please mind that your message will be disregarded nevertheless..')
+      await event.reply('Hej! You have reached out to the automated bot answer, please mind that your message will be disregarded nevertheless..')
 
    @client.on(events.NewMessage(pattern='(?i).*(bot|機器人|机器人)'))
    async def handler(event):
@@ -35,12 +35,12 @@ with TelegramClient(config['user']['session'], config['user']['api_id'], config[
 
    @client.on(events.NewMessage(pattern='(?i).*(阿綱|阿纲)'))
    async def handler(event):
-      if(event.chat_id == config['user']['tom']):
+      if(event.chat_id == config['groups']['tom']):
          await event.reply('阿綱說要請我們所有人吃帝王蟹, 去台灣得頭等艙往返機票也是阿綱全權負責🙈')
 
    @client.on(events.NewMessage(pattern='(?i).*(币圈).*(多久了？)'))
    async def handler(event):
-      if(event.chat_id == config['user']['tom']): #Tooms group
-         await event.reply('見不得人的傢伙, 去死去死去死')
+      if(event.chat_id == config['groups']['tom']): #Tooms group
+         await event.reply('見不得人的緬北無腎俠')
 
    client.run_until_disconnected()
